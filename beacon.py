367fee9f3f3482a3bb641cbe46b7bd1e693b4b81
Dmitriy Panin <dmitriy@backbase.com>
2017-05-15 18:20:56 +0300

    Converted beacon catcher to a thread.

--- beacon.py
+++ beacon.py
@@ -1,11 +1,7 @@
-#!/usr/bin/python
-
 import os
-import subprocess
 import sys
 import struct
 import bluetooth._bluetooth as bluez
-import requests
 import threading
 
 LE_META_EVENT = 0x3e
@@ -14,19 +10,30 @@ OCF_LE_SET_SCAN_ENABLE = 0x000C
 EVT_LE_CONN_COMPLETE = 0x01
 EVT_LE_ADVERTISING_REPORT = 0x02
 
-class BleScanner:
+
+class BleScanner(threading.Thread):
     def __init__(self):
+        threading.Thread.__init__(self)
+        self.__lock = threading.Lock()
+        self.__messages = []
+        self.__sock = self.init_bluetooth()
+
+    def init_bluetooth(self):
         os.system("sudo hciconfig hci0 down")
         os.system("sudo hciconfig hci0 up")
         try:
-            self.__sock = bluez.hci_open_dev(0)
+            return bluez.hci_open_dev(0)
         except:
             sys.exit(1)
 
-    def look_up(self):
+    @staticmethod
+    def packed_bdaddr_to_string(bdaddr_packed):
+        return ':'.join('%02x' % i for i in struct.unpack("<" + "B" * 6, bdaddr_packed[::-1]))
+
+    def run(self):
         self.__sock.getsockopt(bluez.SOL_HCI, bluez.HCI_FILTER, 14)
         cmd_pkt = struct.pack("<BB", 0x01, 0x00)
-        bluez.hci_send_cmd(sock, OGF_LE_CTL, OCF_LE_SET_SCAN_ENABLE, cmd_pkt)
+        bluez.hci_send_cmd(self.__sock, OGF_LE_CTL, OCF_LE_SET_SCAN_ENABLE, cmd_pkt)
 
         while True:
             old_filter = self.__sock.getsockopt(bluez.SOL_HCI, bluez.HCI_FILTER, 14)
@@ -39,17 +46,23 @@ class BleScanner:
             ptype, event, plen = struct.unpack("BBB", pkt[:3])
 
             if event == LE_META_EVENT:
-                subevent, = struct.unpack("B", pkt[3])
+                sub_event, = struct.unpack("B", pkt[3])
                 pkt = pkt[4:]
-                if subevent == EVT_LE_CONN_COMPLETE:
+                if sub_event == EVT_LE_CONN_COMPLETE:
                     le_handle_connection_complete(pkt)
-                elif subevent == EVT_LE_ADVERTISING_REPORT:
-                    mac = packed_bdaddr_to_string(pkt[3:9])
+                elif sub_event == EVT_LE_ADVERTISING_REPORT:
+                    mac = self.packed_bdaddr_to_string(pkt[3:9])
                     rssi = struct.unpack("b", pkt[-1])
+                    self.__lock.acquire()
+                    self.__messages.append((mac, rssi))
+                    self.__lock.release()
                     print(mac, rssi, len(pkt))
 
             self.__sock.setsockopt(bluez.SOL_HCI, bluez.HCI_FILTER, old_filter)
 
-    @staticmethod
-    def packed_bdaddr_to_string(bdaddr_packed):
-        return ':'.join('%02x' % i for i in struct.unpack("<" + "B" * 6, bdaddr_packed[::-1]))
+    def pop_messages(self):
+        self.__lock.acquire()
+        result = self.__messages
+        self.__messages = []
+        self.__lock.release()
+        return result
