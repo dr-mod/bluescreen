b0ffbba2ce9ed93933ea70c9fe0b8085e703e28c
Dmytro Panin <dmytro.panin@gmail.com>
2017-05-17 00:51:23 +0300

    Awake is the last one in the tuple.

--- view.py
+++ view.py
@@ -3,4 +3,4 @@ import observer
 
 class Console(observer.Observer):
     def update(self, device):
-        print 'Name: {:10s}; Awake: {}; Distance: {:f}'.format(*device)
+        print 'Name: {:10s}; Distance: {:f}; Awake: {}'.format(*device)
